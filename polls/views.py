ba069deb3bf7af0c56b67da8a0d00ebc2b21c13d
Jail <jailds07@gmail.com>
2021-07-23 16:17:05 +0600

    update mysite

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.views import generic
 from django.utils import timezone
 from .models import Choice, Question
@@ -27,5 +28,5 @@ class ResultsView(generic.DetailView):
 
 
 def vote(request, question_id):
-    pass
+    return HttpResponse("You're voting on question %s." % question_id)
 
